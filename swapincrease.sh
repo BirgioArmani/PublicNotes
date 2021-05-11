7d8deb631cfbedffa90ccbbc1d29e39b09a0004e
BirgioArmani <64387462+BirgioArmani@users.noreply.github.com>
2021-05-10 22:59:32 -0500

    Update swapincrease.sh
    Added line for installing pv, recommended run script as sudo, however I don't think it's required for installing pv.
    
    Piped dd into pv to show progress bar, so it doesn't seem like nothing is happening.

--- swapincrease.sh
+++ swapincrease.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#This bit installs pv because sitting and looking at nothing sucks.
+
+apt-get install pv -y
+
 # This shell script simply increases swap space to 30GB on gnu linux systems. May take a while to run. 
 
 cd /var/
 touch swapfile
 chmod 600 swapfile
-dd if=/dev/zero of=/var/swapfile bs=1024k count=30000
+dd if=/dev/zero of=/var/swapfile bs=1024k count=30000 | pv -s 30G -n "Expanding swapfile partition"
 mkswap /var/swapfile
 swapon /var/swapfile
